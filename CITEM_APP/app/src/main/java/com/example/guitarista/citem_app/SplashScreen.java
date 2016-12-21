e4fc355458dbd053f5f46d018f3ce063c3dc74a4
SenpaiNoticeMe <lemar_adu@yahoo.com>
2016-12-21 12:06:19 +0800

    New

---
+++ CITEM_APP/app/src/main/java/com/example/guitarista/citem_app/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.guitarista.citem_app;
+
+/**
+ * Created by Guitarista on 21/12/2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        //------------------------------------------------------------------------------------------
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final ImageView iv2 = (ImageView) findViewById(R.id.imageView1);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        final Animation an1 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_in);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        iv.startAnimation(an1);
+        iv2.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
+                iv2.startAnimation(an2);
+                finish();
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+}
