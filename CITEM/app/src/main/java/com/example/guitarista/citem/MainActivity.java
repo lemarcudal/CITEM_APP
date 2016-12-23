ebce6212eea13ff480480e77a8c07f1b19d534c5
SenpaiNoticeMe <lemar_adu@yahoo.com>
2016-12-23 11:07:14 +0800

    Added view

--- CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity
         fragmentTransaction.commit();
         //-------------------------
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
