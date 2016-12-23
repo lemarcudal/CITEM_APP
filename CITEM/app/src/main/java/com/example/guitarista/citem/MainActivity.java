d631a3fb79c50fbfecc309dd25a8595cb405a388
OJT - SMD <smdojt@citem.dti>
2016-12-23 10:07:51 +0800

    2

--- CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         //set the fragment initially
         MainFragment fragment = new MainFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction =
