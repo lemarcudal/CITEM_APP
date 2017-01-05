4a2810beca53cde08383a487f67f2b0b1d946889
OJT - SMD <smdojt@citem.dti>
2017-01-05 12:02:02 +0800

    Update Schedule fragment

--- CITEM/app/src/main/java/com/example/guitarista/citem/GalleryFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/GalleryFragment.java
@@ -1,34 +1,15 @@
 package com.example.guitarista.citem;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.graphics.Color;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.CalendarDayEvent;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GalleryFragment extends Fragment {
-
-    private Toolbar toolbar;
-    CompactCalendarView compactCalendarView;
-    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
-    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
-
 
     public GalleryFragment() {
         // Required empty public constructor
@@ -39,88 +20,7 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_gallery, container, false);
-
-        toolbar = (Toolbar) v.findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        final ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24px);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        // Setting default toolbar title to empty
-        actionBar.setTitle(null);
-
-        compactCalendarView = (CompactCalendarView) v.findViewById(R.id.compactcalendar_view);
-        compactCalendarView.drawSmallIndicatorForEvents(true);
-        compactCalendarView.setUseThreeLetterAbbreviation(true);
-
-        //set initial title
-        actionBar.setTitle(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
-
-        //set title on calendar scroll
-        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
-            @Override
-            public void onDayClick(Date dateClicked) {
-
-
-                Toast.makeText(getActivity(), "Date : " + dateClicked.toString(), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onMonthScroll(Date firstDayOfNewMonth) {
-                // Changes toolbar title on monthChange
-                actionBar.setTitle(dateFormatForMonth.format(firstDayOfNewMonth));
-
-            }
-
-        });
-
-        addDummyEvents();
-
-        gotoToday();
-
-
-
-        return v;
-    }
-
-    // Adding dummy events in calendar view for April, may, june 2016
-    private void addDummyEvents() {
-
-        addEvents(compactCalendarView, Calendar.APRIL);
-        addEvents(compactCalendarView, Calendar.MAY);
-        addEvents(compactCalendarView, Calendar.JUNE);
-
-        // Refresh calendar to update events
-        compactCalendarView.invalidate();
-    }
-
-    private void addEvents(CompactCalendarView compactCalendarView, int month) {
-        currentCalender.setTime(new Date());
-        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
-        Date firstDayOfMonth = currentCalender.getTime();
-        for (int i = 0; i < 6; i++) {
-            currentCalender.setTime(firstDayOfMonth);
-            if (month > -1) {
-                currentCalender.set(Calendar.MONTH, month);
-            }
-            currentCalender.add(Calendar.DATE, i);
-            setToMidnight(currentCalender);
-            compactCalendarView.addEvent(new CalendarDayEvent(currentCalender.getTimeInMillis(), Color.argb(255, 255, 255, 255)), false);
-        }
-    }
-    private void setToMidnight(Calendar calendar) {
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-    }
-    public void gotoToday() {
-
-        // Set any date to navigate to particular date
-        compactCalendarView.setCurrentDate(Calendar.getInstance(Locale.getDefault()).getTime());
-
-
+       return inflater.inflate(R.layout.fragment_gallery, container, false);
     }
 
 
--- CITEM/app/src/main/java/com/example/guitarista/citem/ScheduleFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/ScheduleFragment.java
@@ -2,9 +2,11 @@ package com.example.guitarista.citem;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -12,39 +14,59 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 
-public class ScheduleFragment extends Fragment{
+public class ScheduleFragment extends Fragment implements OnDateSelectedListener, OnMonthChangedListener {
+
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+    @Bind(R.id.calendarView)
+    MaterialCalendarView widget;
+
+    @Bind(R.id.textView)
+    TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+        ButterKnife.bind(getActivity());
 
-        MaterialCalendarView materialCalendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
+        widget.setOnDateChangedListener(this);
+        widget.setOnMonthChangedListener(this);
+        textView.setText(getSelectedDatesString());
 
-        materialCalendarView.state().edit()
-                .setFirstDayOfWeek(Calendar.SUNDAY)
-                .setMinimumDate(CalendarDay.from(1900, 1, 1))
-                .setMaximumDate(CalendarDay.from(2100, 12, 31))
-                .setCalendarDisplayMode(CalendarMode.MONTHS)
-                .commit();
-
-        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                Toast.makeText(getActivity(), "Today is " + date, Toast.LENGTH_SHORT).show();
-            }
-        });
         return v;
-
     }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        textView.setText(getSelectedDatesString());
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(FORMATTER.format(date.getDate()));
+    }
+    private String getSelectedDatesString() {
+        CalendarDay date = widget.getSelectedDate();
+        if (date == null) {
+            return "No Selection";
+        }
+        return FORMATTER.format(date.getDate());
+    }
+
 
 }
