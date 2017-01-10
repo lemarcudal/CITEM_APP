f6eea8c22f4e9dd2ee0d548c67e1e709df71b92a
OJT - SMD <smdojt@citem.dti>
2017-01-10 11:29:18 +0800

    Added Tabhost for Tab Views

--- CITEM/app/src/main/java/com/example/guitarista/citem/ExhibitorFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/ExhibitorFragment.java
@@ -3,6 +3,7 @@ package com.example.guitarista.citem;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +12,29 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExhibitorFragment extends Fragment {
+public class ExhibitorFragment extends Fragment{
+
 
     public ExhibitorFragment() {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exhibitor,container, false);
+       
+        View v = inflater.inflate(R.layout.fragment_exhibitor, container, false);
+
+        FragmentTabHost mTabHost = (FragmentTabHost) v.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getFragmentManager(), R.id.International);
+
+        mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Tab1"),
+                TabInternationalFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("Tab2"),
+                TabLocalFragment.class, null);
+        return v;
     }
 
-
 }
-
-
---
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -0,0 +1,30 @@
+package com.example.guitarista.citem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabInternationalFragment extends Fragment {
+
+
+    public TabInternationalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
+        return v;
+    }
+
+}
