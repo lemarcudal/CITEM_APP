0dfac854fc800c777f1702cdc6864107e8c4aaf0
OJT - SMD <smdojt@citem.dti>
2017-01-11 13:10:46 +0800

    font test

--- CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -1,11 +1,13 @@
 package com.example.guitarista.citem;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,12 +20,19 @@ public class TabInternationalFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    TextView tv1;
+    Typeface f1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_international, container, false);
+        View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
+
+        tv1 = (TextView) v.findViewById(R.id.textView1);
+        f1 = Typeface.createFromAsset(getContext().getAssets(), "century_gothic_bold.ttf");
+        tv1.setTypeface(f1);
+
+        return v;
     }
 
 }
