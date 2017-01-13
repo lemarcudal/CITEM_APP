3ec4f44fadfd8f04b157788b62fb876f542cb6d7
OJT - SMD <smdojt@citem.dti>
2017-01-13 14:19:54 +0800

    Added RecycleView/for testing

---
+++ CITEM/app/src/main/java/com/example/guitarista/citem/ExhibitorInternational.java
@@ -0,0 +1,42 @@
+package com.example.guitarista.citem;
+
+/**
+ * Created by smdojt on 1/13/2017.
+ */
+
+public class ExhibitorInternational {
+
+    private String name;
+
+    private String cathegory;
+
+    private String profilePhotoLocation;
+
+    public ExhibitorInternational() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfilePhotoLocation() {
+        return profilePhotoLocation;
+    }
+
+    public void setProfilePhotoLocation(String profilePhotoLocation) {
+        this.profilePhotoLocation = profilePhotoLocation;
+    }
+
+    public String getCathegory() {
+        return cathegory;
+    }
+
+    public void setCathegory(String cathegory) {
+        this.cathegory = cathegory;
+    }
+}
--- CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -1,12 +1,18 @@
 package com.example.guitarista.citem;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,6 +25,9 @@ public class TabInternationalFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,7 +36,28 @@ public class TabInternationalFragment extends Fragment {
         TextView txt = (TextView) v.findViewById(R.id.textView);
         Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
         txt.setTypeface(font);
+
+        ArrayList<ExhibitorInternational> itemList = new ArrayList<>();
+        fillDummyData(itemList);
+
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+
+        itemAdapter = new ItemAdapter(itemList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
         return v;
     }
+
+    private void fillDummyData(ArrayList<ExhibitorInternational> itemList) {
+        ExhibitorInternational exInt1 = new ExhibitorInternational();
+        exInt1.setName("ABEEBA CORPORATION");
+        exInt1.setCathegory("FASHION");
+        exInt1.setProfilePhotoLocation("http://www.cgtutorials.com/oneadmin/_files/linksdir/9041_turn_the_low_quality_photos_to_high_quality.jpg");
+        itemList.add(exInt1);
+    }
+
 
 }
