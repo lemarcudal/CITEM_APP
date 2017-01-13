372ab483661e8f0bb244fa5403e0070a328a831e
OJT - SMD <smdojt@citem.dti>
2017-01-13 13:03:31 +0800

    text

--- CITEM/app/src/main/java/com/example/guitarista/citem/ContactFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/ContactFragment.java
@@ -1,11 +1,13 @@
 package com.example.guitarista.citem;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -24,7 +26,22 @@ public class ContactFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getActivity().setTitle("Contact Us");
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
+        TextView txt1 = (TextView) v.findViewById(R.id.textView1);
+        TextView txt2 = (TextView) v.findViewById(R.id.textView2);
+        TextView txt3 = (TextView) v.findViewById(R.id.textView3);
+        TextView txt4 = (TextView) v.findViewById(R.id.textView4);
+        TextView txt5 = (TextView) v.findViewById(R.id.textView5);
+        TextView txt6 = (TextView) v.findViewById(R.id.textView6);
+        Typeface font1 = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
+        Typeface font2 = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
+        txt1.setTypeface(font1);
+        txt2.setTypeface(font2);
+        txt3.setTypeface(font1);
+        txt4.setTypeface(font2);
+        txt5.setTypeface(font1);
+        txt6.setTypeface(font2);
+        return v;
     }
 
 }
