efe9597c5e8cf6916e752fbe33873862cbd3aeeb
SenpaiNoticeMe <lemar_adu@yahoo.com>
2017-01-13 10:14:07 +0800

    about font test/Success

--- CITEM/app/src/main/java/com/example/guitarista/citem/AboutFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/AboutFragment.java
@@ -1,11 +1,14 @@
 package com.example.guitarista.citem;
 
 
+import android.graphics.Typeface;
+import android.icu.text.DisplayContext;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -24,7 +27,16 @@ public class AboutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getActivity().setTitle("About Us");
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView txt1 = (TextView) v.findViewById(R.id.textView1);
+        TextView txt2 = (TextView) v.findViewById(R.id.textView2);
+        TextView txt3 = (TextView) v.findViewById(R.id.textView3);
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
+        Typeface font1 = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_regular.ttf");
+        txt1.setTypeface(font);
+        txt2.setTypeface(font1);
+        txt3.setTypeface(font1);
+        return v;
     }
 
 }
