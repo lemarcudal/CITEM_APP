ad48c5f082c0a44f32be7df01911858204e769a1
OJT - SMD <smdojt@citem.dti>
2017-01-18 16:56:06 +0800

    Updated TabIntFragment codes/ incomplete

--- CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -10,9 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
@@ -20,6 +22,11 @@ import java.util.ArrayList;
  */
 public class TabInternationalFragment extends Fragment {
 
+    String[] players={"Player1","Player2", "Player3"};
+    int[] images = {R.drawable.custom_mf_logo,R.drawable.custom_mf_logo,R.drawable.custom_mf_logo};
+
+    ArrayList<HashMap<String, String>> data;
+    SimpleAdapter adapter;
 
     public TabInternationalFragment() {
         // Required empty public constructor
@@ -31,8 +38,22 @@ public class TabInternationalFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
 
-
+        //code for fill
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (int i=0;i<players.length;i++)
+        {
+            map = new HashMap<String, String>();
+            map.put("Player", players[i] );
+            map.put("Image", Integer.toString(images[i]));
+            data.add(map);
+        }
+        String[] from={"Player", "Image"};
+        int[] to = {R.id.nameTxt, R.id.imageView1};
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.fragment_tab_international, from, to);
+        setListAdapter(adapter);
+        //--------------------------------------------
         return v;
     }
+
 
 }
