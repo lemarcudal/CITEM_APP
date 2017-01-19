c68f2d95c9bfe0ee2ae94e23c672b6a6149bbebb
OJT - SMD <smdojt@citem.dti>
2017-01-19 11:28:13 +0800

    Check state

--- CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -1,36 +1,21 @@
 package com.example.guitarista.citem;
 
-import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TabInternationalFragment extends ListFragment {
+public class TabInternationalFragment extends Fragment {
 
-    String[] players={"Player1","Player2", "Player3"};
-    int[] images = {R.drawable.custom_mf_logo,R.drawable.custom_mf_logo,R.drawable.custom_mf_logo};
-
-    ArrayList<HashMap<String, String>> data=new ArrayList<HashMap<String, String>>() ;
-    SimpleAdapter adapter;
 
     public TabInternationalFragment() {
         // Required empty public constructor
@@ -41,39 +26,11 @@ public class TabInternationalFragment extends ListFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
+        TextView txt = (TextView) v.findViewById(R.id.textView);
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
+        txt.setTypeface(font);
 
-        //code for fill
-        HashMap<String, String> map = new HashMap<String, String>();
-        for (int i=0;i<players.length;i++)
-        {
-            map = new HashMap<String, String>();
-            map.put("Player", players[i] );
-            map.put("Image", Integer.toString(images[i]));
-            data.add(map);
-        }
-        String[] from={"Player", "Image"};
-        int[] to = {R.id.nameTxt, R.id.imageView1};
-        adapter = new SimpleAdapter(getActivity(), data, R.layout.model, from, to);
-        setListAdapter(adapter);
-        //--------------------------------------------
         return v;
-
-
     }
-
-    @Override
-    public void onStart(){
-        super.onStart();
-
-        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(), data.get(position).get("Player"), Toast.LENGTH_SHORT);
-            }
-        });
-    }
-
-
 
 }
