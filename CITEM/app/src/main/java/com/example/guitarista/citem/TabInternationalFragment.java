11066e7f034b198968f4780cffd37883d1aeda43
OJT - SMD <smdojt@citem.dti>
2017-01-19 11:40:42 +0800

    Revert to working state

--- CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabInternationalFragment.java
@@ -1,5 +1,6 @@
 package com.example.guitarista.citem;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -26,9 +27,12 @@ public class TabInternationalFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tab_international, container, false);
-        TextView txt = (TextView) v.findViewById(R.id.textView);
-        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
-        txt.setTypeface(font);
+        TextView txt = (TextView) v.findViewById(R.id.person_name);
+        TextView txt1 = (TextView) v.findViewById(R.id.person_age);
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_regular.ttf");
+        Typeface font1 = Typeface.createFromAsset(getActivity().getAssets(), "century_gothic_bold.ttf");
+        txt.setTypeface(font1);
+        txt1.setTypeface(font);
 
         return v;
     }
