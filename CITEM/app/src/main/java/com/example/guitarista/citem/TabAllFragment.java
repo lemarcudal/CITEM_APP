2c339d368ab95ec541a555a49d9360f91aa7c384
OJT - SMD <smdojt@citem.dti>
2017-01-25 13:34:35 +0800

    ListView sample for attendees_all

--- CITEM/app/src/main/java/com/example/guitarista/citem/TabAllFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabAllFragment.java
@@ -1,11 +1,15 @@
 package com.example.guitarista.citem;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 /**
@@ -17,13 +21,47 @@ public class TabAllFragment extends Fragment {
     public TabAllFragment() {
         // Required empty public constructor
     }
-
+    ListView list;
+    String[] AllAttendees = {
+            "Google Plus",
+            "Twitter",
+            "Windows",
+            "Bing",
+            "Itunes",
+            "Wordpress",
+            "Drupal"
+    };
+    int[] imageID = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_all, container, false);
+        View v = inflater.inflate(R.layout.fragment_tab_all, container, false);
+
+        TabAllList adapter = new
+                TabAllList(TabAllFragment.this.getActivity(), AllAttendees, imageID);
+        list = (ListView) v.findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(getActivity(), SplashScreen.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        return v;
     }
 
 }
---
+++ CITEM/app/src/main/java/com/example/guitarista/citem/TabAllList.java
@@ -0,0 +1,37 @@
+package com.example.guitarista.citem;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+/**
+ * Created by smdojt on 1/25/2017.
+ */
+
+public class TabAllList extends ArrayAdapter<String>{
+
+    private final Activity context;
+    private final String[] AllAttendees;
+    private final int[] imageId;
+    public TabAllList(Activity context,
+                      String[] AllAttendess, int[] imageId) {
+        super(context, R.layout.attall_list_single, AllAttendess);
+        this.context = context;
+        this.AllAttendees = AllAttendess;
+        this.imageId = imageId;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent){
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.attall_list_single, null, true);
+        TextView txtName = (TextView) rowView.findViewById(R.id.txtName);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtName.setText(AllAttendees[position]);
+        imageView.setImageResource(Integer.parseInt(String.valueOf(imageId[position])));
+        return rowView;
+    }
+}
