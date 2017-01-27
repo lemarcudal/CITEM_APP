5f68a00c2f95b4ec023be6f85a6146a2a0a71dc7
OJT - SMD <smdojt@citem.dti>
2017-01-27 15:15:23 +0800

    Test glide image gallery

---
+++ CITEM/app/src/main/java/com/example/guitarista/citem/Gallery/activity/GalleryFragment.java
@@ -0,0 +1,147 @@
+package com.example.guitarista.citem.Gallery.activity;
+
+
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.guitarista.citem.Gallery.adapter.GalleryAdapter;
+import com.example.guitarista.citem.Gallery.app.AppController;
+import com.example.guitarista.citem.Gallery.model.Image;
+import com.example.guitarista.citem.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GalleryFragment extends Fragment {
+
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    private String TAG = GalleryFragment.class.getSimpleName();
+    private static final String endpoint = "http://api.androidhive.info/json/glide.json";
+    private ArrayList<Image> images;
+    private ProgressDialog pDialog;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getActivity().setTitle("GALLERY");
+        View v = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+
+        pDialog = new ProgressDialog(getActivity());
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(getContext().getApplicationContext(), images);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext().getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        recyclerView.addOnItemTouchListener(new GalleryAdapter.RecyclerTouchListener(getContext().getApplicationContext(), recyclerView, new GalleryAdapter.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("images", images);
+                bundle.putInt("position", position);
+
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                SlideshowDialogFragment newFragment = SlideshowDialogFragment.newInstance();
+                newFragment.setArguments(bundle);
+                newFragment.show(ft, "slideshow");
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        fetchImages();
+        return v;
+    }
+
+    private void fetchImages() {
+
+        pDialog.setMessage("Downloading json...");
+        pDialog.show();
+
+        JsonArrayRequest req = new JsonArrayRequest(endpoint,
+                new Response.Listener<JSONArray>() {
+
+                    @TargetApi(Build.VERSION_CODES.M)
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+
+                        images.clear();
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                Image image = new Image();
+                                image.setName(object.getString("name"));
+
+                                JSONObject url = object.getJSONObject("url");
+                                image.setSmall(url.getString("small"));
+                                image.setMedium(url.getString("medium"));
+                                image.setLarge(url.getString("large"));
+                                image.setTimestamp(object.getString("timestamp"));
+
+                                images.add(image);
+
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                            }
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                pDialog.hide();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+
+}
