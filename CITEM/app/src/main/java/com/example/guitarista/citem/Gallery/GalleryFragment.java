5769932b3f7b52e33e6cb47c11ff477c4b844e24
SenpaiNoticeMe <lemar_adu@yahoo.com>
2017-01-30 17:20:40 +0800

    Restore Point 4

--- CITEM/app/src/main/java/com/example/guitarista/citem/Gallery/GalleryFragment.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/Gallery/GalleryFragment.java
@@ -32,16 +32,13 @@ public class GalleryFragment extends Fragment {
     ArrayList<ImageModel> data = new ArrayList<>();
 
     public static String IMGS[] = {
-            "https://images.unsplash.com/photo-1444090542259-0af8fa96557e?q=80&fm=jpg&w=1080&fit=max&s=4b703b77b42e067f949d14581f35019b",
-            "https://images.unsplash.com/photo-1439546743462-802cabef8e97?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1441155472722-d17942a2b76a?q=80&fm=jpg&w=1080&fit=max&s=80cb5dbcf01265bb81c5e8380e4f5cc1",
-            "https://images.unsplash.com/photo-1437651025703-2858c944e3eb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1431538510849-b719825bf08b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1434873740857-1bc5653afda8?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1439396087961-98bc12c21176?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1433616174899-f847df236857?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438480478735-3234e63615bb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438027316524-6078d503224b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
+            "http://www.manilafame.com/en/images/gallery/springforward/01.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/02.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/03.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/04.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/05.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/06.jpg",
+            "http://www.manilafame.com/en/images/gallery/springforward/07.jpg",
             "http://cms.interaksyon.com/lifestyle/assets/2014/02/PCM_9925-2.jpg"
     };
 
--- CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
+++ CITEM/app/src/main/java/com/example/guitarista/citem/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.guitarista.citem;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,10 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.guitarista.citem.Attendees.AttendeesFragment;
 import com.example.guitarista.citem.Exhibitor.ExhibitorFragment;
 import com.example.guitarista.citem.Gallery.GalleryFragment;
+
 
 
 public class MainActivity extends AppCompatActivity
@@ -59,7 +62,7 @@ public class MainActivity extends AppCompatActivity
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
+    //default back pressed / New Back pressed below
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -69,6 +72,8 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+
+
 
     //----------------The code below displays the 3 dotted settings in the upper right of action bar-----------
     //@Override
